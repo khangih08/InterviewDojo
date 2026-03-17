f7dedc2f6dcb72e1fee4c00f2c88647e7076f54e
khangih08 <handlele08@gmail.com>
2026-03-17 11:38:08 +0700

    add file backend

---
+++ backend/backend/dist/entities/user.entity.d.ts
@@ -0,0 +1,9 @@
+export declare class User {
+    id: string;
+    email: string;
+    pássword: string;
+    full_name: string;
+    target_role: string;
+    experience_level: string;
+    role: string;
+}
