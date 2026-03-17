f9768d0a42094b1b347f9ecb89a9202f0615c234
khangih08 <handlele08@gmail.com>
2026-03-17 09:57:27 +0700

    create user, category, tag entities

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('users')
+export class User {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({unique: true})
+    email: string;
+
+    @Column({select: false})
+    pássword: string;
+
+    @Column()
+    full_name: string;
+
+    @Column()
+    target_role: string;
+
+    @Column()
+    experience_level: string;
+
+    @Column({default: 'candidate'})
+    role: string;
+};
