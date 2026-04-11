93148dc7af8ad3b79476cb6a1f475bfb367c33b6
khanhtaduy2k5 <khanhtaduy2k5@gmail.com>
2026-04-11 16:46:55 +0700

    2

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,45 +6,28 @@ import { User } from './entities/user.entity';
 import { Category } from './entities/category.entity';
 import { Tag } from './entities/tag.entity';
 import { Question } from './entities/question.entity';
-import { TagRelation } from './entities/tag_relation.entity'; 
+import { TagRelation } from './entities/tag_relation.entity';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { CategoriesModule } from './categories/categories.module';
 import { TagsModule } from './tag/tags.module';
 import { QuestionsModule } from './questions/questions.module';
-
 import { MulterModule } from '@nestjs/platform-express';
-
 import { InterviewsController } from './interviews/controller';
 import { InterviewsService } from './interviews/service';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true, 
+      isGlobal: true,
     }),
-MulterModule.register({
+    MulterModule.register({
       dest: './uploads',
     }),
     AuthModule,
     CategoriesModule,
     TagsModule,
     QuestionsModule,
-/*
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [User, Category, Tag, TagRelation, Question],
-      synchronize: true,
-      logging: true,
-    }),
-  ],
-  controllers: [AppController,InterviewsController],
-  providers: [AppService,InterviewsService],
-})*/
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
@@ -53,11 +36,13 @@ MulterModule.register({
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       ssl:
-      process.env.DB_SSL === 'true'
-        ? { rejectUnauthorized: false }
-        : false,
-    entities: [User, Category, Tag, TagRelation, Question],
-    synchronize: true,
-    logging: true,
-  })
+        process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
+      entities: [User, Category, Tag, TagRelation, Question],
+      synchronize: true,
+      logging: true,
+    }),
+  ],
+  controllers: [AppController, InterviewsController],
+  providers: [AppService, InterviewsService],
+})
 export class AppModule {}
